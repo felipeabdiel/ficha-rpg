d6761d4829d1ca79360b8ed7a7098159158b26bf
felipeabdiel <felipe_brito@discente.ufg.br>
2024-06-18 13:05:00 -0300

    Início do Projeto

---
+++ js/personagem.js
@@ -0,0 +1,12 @@
+class Personagem{
+    constructor(nome, forca, destreza, constituicao, inteligencia, sabedoria, carisma){
+        this.nome = nome;
+        this.forca = forca;
+        this.destreza = destreza;
+        this.constituicao = constituicao;
+        this.inteligencia = inteligencia;
+        this.sabedoria = sabedoria;
+        this.carisma = carisma;
+    }
+
+}
